b121e7ddbc25cc27a2fe1dc0e6ce7943f18047d2
VictorMagosso <victor_magosso@hotmail.com>
2020-07-30 19:19:55 -0300

    Conversor funcionando

--- scripts/conversor.js
+++ scripts/conversor.js
@@ -1,13 +1,24 @@
-let valorDolar = document.querySelector('#valorDolar').value
-let valorReal = document.querySelector('#valorReal').value
-let dolarAuxiliar = ''
 
-const buttonCalcular =  () => {
+let dolarAuxiliar = parseFloat()
+let dolarConvertido = parseFloat()
+let valorDolar = document.querySelector('#valorDolar')
+let valorReal = document.querySelector('#valorReal')
+
+const recuperarDolar =  () => {
     fetch('https://economia.awesomeapi.com.br/json/all/USD-BRL')
             .then(res => res.json()
-                .then(data => dolarAuxiliar = Number(data.USD.ask)))
+                .then(data => dolarAuxiliar = data.USD.ask))
             .catch(e => alert(`Deu erro :( - ${e.message}`))  
 
-    valorDolar = dolarAuxiliar*valorReal 
-    alert(valorDolar)
+    // valorDolar = dolarAuxiliar*valorReal 
+
 }
+const conRealDolar = (valorReal) => {
+    dolarConvertido = (valorReal*dolarAuxiliar).toFixed(2)
+    valorDolar.value = dolarConvertido.toString()
+}
+const conDolarReal = (valorDolar) => {
+    realConvertido = (valorDolar/dolarAuxiliar).toFixed(2)
+    valorReal.value = realConvertido.toString()
+}
+recuperarDolar()
